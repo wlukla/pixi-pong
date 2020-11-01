260cd6b41a8df37dd898b0c515e712ed1cf680da
wlukla <vladislav.danilochkin@gmail.com>
2020-11-01 12:27:56 +0200

    feat: try image loading

--- src/script.js
+++ src/script.js
@@ -1,14 +1,30 @@
 import * as PIXI from 'pixi.js';
 
-let type = "WebGL";
+//Create a Pixi Application
+let app = new PIXI.Application({ 
+  width: 256, 
+  height: 256,                       
+  antialias: true, 
+  transparent: false, 
+  resolution: 1
+});
 
-if  (!PIXI.utils.isWebGLSupported())  {
-  type = "canvas";
-}
-
-let app = new PIXI.Application({width: 256, height: 256});
+const imgUrl = 'https://raw.githubusercontent.com/kittykatattack/learningPixi/master/examples/images/cat.png';
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
 
-PIXI.utils.sayHello(type);
+//load an image and run the `setup` function when it's done
+app.loader
+  .add('catImage', imgUrl)
+  .load(setup);
+
+//This `setup` function will run when the image has loaded
+function setup() {
+
+  //Create the cat sprite
+  let cat = new PIXI.Sprite(app.loader.resources.catImage.texture);
+
+  //Add the cat to the stage
+  app.stage.addChild(cat);
+}
